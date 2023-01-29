f54f89a6449db399e8f13370aa07e167084b650b
Adam Smelkowski <prv@smelkowski.loc>
2023-01-29 20:14:03 +0100

    Clean up

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 WALLPAPERS_DIR=$HOME/Wallpapers
 
 ls $WALLPAPERS_DIR |sort -R |tail -1 |while read file; do
-    wal -i $WALLPAPERS_DIR/$file -q
+    echo "$WALLPAPERS_DIR/$file" >> $HOME/dev/dotfiles/scripts/wallpapers.log 2>&1
+    /home/prv/.pyenv/shims/wal -i $WALLPAPERS_DIR/$file >> $HOME/dev/dotfiles/scripts/wallpapers.log 2>&1
 done
